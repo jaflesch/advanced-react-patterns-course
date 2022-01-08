366f78233b96b0bfef132803cf59d22fbe2ea74d
Jean Flesch <jean.flesch93@gmail.com>
2022-01-08 11:19:04 -0300

    Add hooks folder index file

---
+++ src/patterns/hooks/index.js
@@ -0,0 +1,2 @@
+export {default as useClapAnimation} from './useClapAnimation';
+export {default as useDOMRef} from './useDOMRef';
--- src/patterns/hooks/useClapAnimation.js
+++ src/patterns/hooks/useClapAnimation.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import mojs from 'mo-js';
 
 const useClapAnimation = ({
