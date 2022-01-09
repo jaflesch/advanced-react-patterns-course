e1b5d8457dd5f5a76e9828f70d9be130ddd453f0
Jean Flesch <jean.flesch93@gmail.com>
2022-01-09 11:56:47 -0300

    Add usePrevious hook

--- src/patterns/hooks/index.js
+++ src/patterns/hooks/index.js
@@ -2,3 +2,4 @@ export {default as useClapAnimation} from './useClapAnimation';
 export {default as useDOMRef} from './useDOMRef';
 export {default as useClapState} from './useClapState';
 export {default as useAfterMount} from './useAfterMount';
+export {default as usePrevious} from './usePrevious';
